6ea463850f72f91ce22fd19818d82336024476e9
Rodrigo Xavier <rodrigomatiasxavier@gmail.com>
2021-09-11 15:24:52 -0300

    corrigindo bug

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -16,7 +16,7 @@ class ArticleSerializer(serializers.ModelSerializer):
 
     def set_keywords(self, article, keywords):
         for k in keywords:
-            keyword = models.Keyword.objects.get_or_create(name=k)
+            keyword, created = models.Keyword.objects.get_or_create(name=k)
             article.keyword_set.add(keyword)
     
     def create(self, validated_data):
@@ -25,9 +25,7 @@ class ArticleSerializer(serializers.ModelSerializer):
         keywords = self.context['request'].data['keywords']
         self.set_keywords(article, keywords)
 
-        return super().create(validated_data)
-
-        # return article
+        return article
     class Meta:
         model = models.Article
         fields = ['title', 'subtitle', 'article_type', 'content', 'status', 'keyword_set']
